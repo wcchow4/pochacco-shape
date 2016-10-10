1a9a461faddf46e5e7366e35ec154fe05d40d648
Wai Chung Chow <Wai Chung Chow>
2016-10-10 21:39:12 +0800

    simple svg animation done

---
+++ app.js
@@ -0,0 +1,7 @@
+setTimeout(function() {
+  var paths = document.getElementsByClassName("st1");
+  for (var i=0;i<paths.length;i++) {
+    paths[i].classList.add('show');
+  }
+  console.log(paths);
+}, 5000);
